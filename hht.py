94870eee28eae54c42c7a17dd09593e53b436225
christineyan4 <cyan4@uchicago.edu>
2019-10-27 18:41:07 -0500

    updated hht w frequencies

--- hht.py
+++ hht.py
@@ -5,47 +5,54 @@ from playsound import playsound
 import time
 import csv
 
+def generate_pid():
+    randomletter = chr(random.randint(65, 90))
+    randomint = str(random.randint(1, 10))
+    pid = randomletter + randomint
+    return pid
+
 def prescreen(pid, pswriter):
+    print("Welcome to our study. We will be playing different freqs at varying volumes. There will be a prescreen, followed by a test tone before beginning the hearing test. Please do not adjust the volume.")
+
     age = input("How old are you?\n")
     issues = input("Have you ever had any hearing-related issues (ex: hearing loss, ear infections)? Y/N\n")
+
+    print("Playing test tone...\n")
+    playsound("audio/test.mp3")
+    input("Press enter to begin.\n")
+
     pswriter.writerow([pid, age, issues])
 
-def hearing_test(pid, audiofiles, htwriter):
-    for i in range(len(audiofiles)):
-        print("Playing sound")
-        wait = time.sleep(random.randint(1, 5))
+def hearing_test(pid, freqs, htwriter):
+    for i in range(len(freqs)):
+        print("Playing sound...\n")
+        wait = random.randint(1, 5)
+        time.sleep(wait)
 
-        x = random.randint(0, len(audiofiles) - 1)
-        playsound('audio/' + audiofiles[x] + 'Hz.mp3')
+        x = random.randint(0, len(freqs) - 1)
+        playsound('audio/' + freqs[x] + 'Hz.mp3')
         time.sleep(5 - wait)
 
         rate = input("On a scale of 0 - 4 how clearly did you hear the sound? 0 - I did not hear anything, 4 - I heard it very clearly\n")
-        htwriter.writerow([pid, audiofiles[x], rate])
-        audiofiles.pop(x)
+        htwriter.writerow([pid, freqs[x], rate])
+        freqs.pop(x)
 
 if __name__ == '__main__':
-    randletter = chr(random.randint(65, 90))
-    randint = str(random.randint(1, 10))
-    pid = randletter + randint
-    print(pid)
-
-    audiofiles = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
-    print("Welcome to our study. We will be playing different frequencies at varying volumes. There will be a prescreen, followed by a test tone before beginning the hearing test. Please do not adjust the volume.")
+    pid = generate_pid()
+    freqs = ['1000', '100', '120', '14000', '140', '160', '18000', '180', '20000', '200', '20', '3000', '30', '400', '40', '5000', '50', '600', '60', '7000', '70', '800', '80', '9000']
 
     if os.path.isfile('prescreen.csv'):
         f = open('prescreen.csv', 'a')
     else:
         f = open('prescreen.csv', 'w')
+
     pswriter = csv.writer(f, delimiter=",")
     prescreen(pid, pswriter)
-
-    print("Playing test tone...\n")
-    # playsound("test.mp3")
-    input("Press enter to begin.\n")
 
     if os.path.isfile('results.csv'):
         f = open('results.csv', 'a')
     else:
         f = open('results.csv', 'w')
+
     htwriter = csv.writer(f, delimiter=",")
-    hearing_test(pid, audiofiles, htwriter)
+    hearing_test(pid, freqs, htwriter)
